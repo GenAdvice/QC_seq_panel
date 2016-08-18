25f73a0a3324fb87afbc23d7ae809fa7c0710b05
mmoisse Oshii <matthieu.moisse@Gmail.com>
2016-08-18 13:55:31 +0200

    Initial commit

--- init.env.sh
+++ init.env.sh
@@ -8,7 +8,7 @@ export GenAdviceHome=`pwd`
 ## Get the right software
 bash software/getSoftware.sh
 
-export PATH="${GenAdviceHome}/Ysoftware/anaconda/anaconda_3-4.1.1/anaconda3/bin/:$PATH"
+export PATH="${GenAdviceHome}/software/anaconda/anaconda_3-4.1.1/anaconda3/bin/:$PATH"
 
 ## Get the right ref genome
 bash databases/hg19/get.hg19.sh
